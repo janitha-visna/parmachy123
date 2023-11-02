f5d96e40dcf8db0074abd745404e5b71f06d7a06
janitha <visnajanitha@gmail.com>
2023-11-02 20:54:24 +0530

    1 video code

---
+++ src/main/java/com/inn/cafe/CafeMangementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.inn.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeMangementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeMangementSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/inn/cafe/constents/CafeConstants.java
@@ -0,0 +1,6 @@
+package com.inn.cafe.constents;
+
+public class CafeConstants {
+    public  static  final String Something_went_wrong="something Went Wrong.";
+    public static final String INAVLID_DATA="Invaild Data.";
+}
